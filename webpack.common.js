36d86829c1f8291e95d001619221c1d1456ec45d
exthirix <exthirix@gmail.com>
2022-03-13 00:48:15 +0900

    public pathの設定漏れを追加

--- webpack.common.js
+++ webpack.common.js
@@ -25,6 +25,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
   },
   watchOptions: {
     ignored: /node_modules/,
